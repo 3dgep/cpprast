f6251380f194a30d6efafc2977f1212922ae5554
Jeremiah van Oosten <jeremiah@3dgep.com>
2026-07-01 14:25:03 +0200

    Refactor Rasterizer::drawSprite function.

--- graphics/src/Rasterizer.cpp
+++ graphics/src/Rasterizer.cpp
@@ -17,14 +17,11 @@ void Rasterizer::drawSprite( const Sprite& sprite, int _x, int _y )
     if ( !srcImage || !dstImage )
         return;
 
-    const Color      color     = sprite.getColor();
-    const BlendMode  blendMode = sprite.getBlendMode();
-    const AABB       clipAABB  = AABB::fromRect( state.clipRect );
-    const AABB       dstAABB   = dstImage->getAABB().clamped( clipAABB );
-    const glm::ivec2 size      = sprite.getSize();
-    glm::ivec2       uv        = sprite.getUV();
+    const AABB       clipAABB = AABB::fromRect( state.clipRect );
+    const AABB       dstAABB  = dstImage->getAABB().clamped( clipAABB );
+    const glm::ivec2 size     = sprite.getSize();
 
-    // Compute viewport clipping bounds.
+    // Compute clipping bounds.
     const int clipLeft   = std::max( static_cast<int>( dstAABB.min.x ), _x );
     const int clipTop    = std::max( static_cast<int>( dstAABB.min.y ), _y );
     const int clipRight  = std::min( static_cast<int>( dstAABB.max.x ), _x + size.x - 1 );
@@ -35,22 +32,25 @@ void Rasterizer::drawSprite( const Sprite& sprite, int _x, int _y )
         return;
 
     // Adjust sprite UV based on clipping.
+    glm::ivec2 uv = sprite.getUV();
     uv.x += clipLeft - _x;
     uv.y += clipTop - _y;
+
+    const Color     color     = sprite.getColor();
+    const BlendMode blendMode = sprite.getBlendMode();
+    int sW = srcImage->getWidth();  // Source image width.
+    int dW = dstImage->getWidth();  // Destination image width.
 
     const Color* src = srcImage->data();
     Color*       dst = dstImage->data();
 
-    int sW = srcImage->getWidth();  // Source image width.
-    int dW = dstImage->getWidth();  // Destination image width.
-
     for ( int y = clipTop; y <= clipBottom; ++y )
     {
+        int v = uv.y + ( y - clipTop );
         for ( int x = clipLeft; x <= clipRight; ++x )
         {
             // Compute clipped UV sprite texture coordinates.
             int u = uv.x + ( x - clipLeft );
-            int v = uv.y + ( y - clipTop );
 
             Color sC = src[v * sW + u] * color;
             Color dC = dst[y * dW + x];
